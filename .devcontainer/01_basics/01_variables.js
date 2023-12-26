6f353d09b8f1e3340bda834533ba479fa6e402d9
Abhi1818-18 <131249108+Abhi1818-18@users.noreply.github.com>
2023-12-26 06:10:19 +0000

    Configuration done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 1234;
+let accountEmail = "abh18@gmail.com"
+var accountPassword = "abh198"
+accountCity = "pune"
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
