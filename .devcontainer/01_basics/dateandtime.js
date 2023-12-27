88bf38d3bfc96ba1d55dd0e5eaebb22307048ddb
Abhi1818-18 <131249108+Abhi1818-18@users.noreply.github.com>
2023-12-27 16:31:34 +0000

    dateandtime file added.

---
+++ .devcontainer/01_basics/dateandtime.js
@@ -0,0 +1,11 @@
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toDateString ())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleString())
+console.log(typeof myDate)
+let myCreatedDate = new Date (2023,0,23)
+console.log(myCreatedDate)
+
+console.log(Date.now())
+
