b9f109a75dc4380d0ab16a49bbc37d9258f8d190
Abhi1818-18 <131249108+Abhi1818-18@users.noreply.github.com>
2023-12-27 07:14:36 +0000

    math file added

---
+++ .devcontainer/01_basics/03_Numberandmath.js
@@ -0,0 +1,31 @@
+const score = 400;
+//console.log(score);
+
+const recharge = new Number(250)
+//console.log(recharge);
+
+console.log(recharge.toString().length) // converts number to string
+//console.log(recharge.toFixed(1))
+
+const otherNumber = 1123.56
+
+//console.log(otherNumber.toPrecision(4)); //provide precision upto given values
+
+const hundreds = 10000000000
+//console.log(hundreds.toLocaleString('en-IN')); //provide and seperates value as per indian standards
+
+   //         ****maths******
+
+console.log(Math)
+
+console.log(Math.abs(-4));
+console.log(Math.floor(5.7));
+console.log(Math.ceil(5.2));
+console.log(Math.round(5.5));
+
+
+
+
+
+
+
