47e41386c43d85566318e93a1fe694a9dda0a64c
Abhi1818-18 <131249108+Abhi1818-18@users.noreply.github.com>
2023-12-29 02:38:47 +0000

    changes done

--- .devcontainer/01_basics/arrays.js
+++ .devcontainer/01_basics/arrays.js
@@ -6,11 +6,11 @@ let car = cars[1];
 //console.log(car);
 const fruits = ["Banana", "Orange", "Apple"];
 fruits.push("Lemon"); //adds lemon element to array at last position
-console.log(fruits);
+//console.log(fruits);
 
 const fruits1 = ["Banana", "Orange", "Apple"];
 fruits[6] = "Lemon";  // Creates undefined "holes" in fruits
-console.log(fruits1);
+//console.log(fruits1);
 
 //In JavaScript, arrays always use numbered indexes.
 // If you use named indexes, JavaScript will redefine the array to an object.  
@@ -22,13 +22,36 @@ You should use arrays when you want the element names to be numbers.
 */
 
 const cricketers = ["Virat","Rohit","ABD","Dhoni"]
-console.log(cricketers.length); //to check length of array
+//console.log(cricketers.length); //to check length of array
 
-console.log(Array.isArray(cricketers)) //to check whether array or not
+//console.log(Array.isArray(cricketers)) //to check whether array or not
 
-console.log(cricketers.toString())//The JavaScript method toString() converts an array to a string of (comma separated) array values.
+//console.log(cricketers.toString())//The JavaScript method toString() converts an array to a string of (comma separated) array values.
 
-console.log(cricketers.join('-')) /*The join() method also joins all array elements into a string.
- It behaves just like toString(), but in addition you can specify the separator:*/
+//console.log(cricketers.join('-')) /*The join() method also joins all array elements into a string.
+ //It behaves just like toString(), but in addition you can specify the separator:*/
 
+ let numbers = [1,5,8,3,6]
+ let reversedArray = numbers.reverse(); //returns element in reverse order
+ console.log(reversedArray);
+
+ console.log(numbers); // reverse method modifies the original array
+
+ let languages = ["JavaScript", "Python", "C++", "Java", "Lua"];
+
+ let reversedArray1 = [...languages].reverse(); //if we use spread operator it doesnt modify original array
+
+ console.log(reversedArray1);
+ console.log(languages);
+
+ const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
+
+ const result = words.filter((word)=>word.length>6) 
+ console.log(result);
+
+ const array1 = [5, 12, 8, 130, 44];
+
+const found = array1.find((element) => element > 10);
+
+console.log(found);
  
