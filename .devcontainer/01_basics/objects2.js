27cd77c29d27b1b3c1fbd1d1001b20f4fb3bcd66
Abhi1818-18 <131249108+Abhi1818-18@users.noreply.github.com>
2023-12-29 07:12:14 +0000

    objects2 file added

---
+++ .devcontainer/01_basics/objects2.js
@@ -0,0 +1,36 @@
+const tinderUser = new Object()
+
+tinderUser.id = "Abc123"
+tinderUser.name = "Abhishek"
+tinderUser.location = "pune"
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser))   //provide object values in array form we can use loops on this
+console.log(Object.entries(tinderUser));
+
+console.log(Object.hasOwnProperty('loction')); //tells whether certain property exist or not
+
+
+const user ={
+    email : "abhi@gmail.com",
+    fullName:{
+        Userfullname:{
+            name:"abhishek gurav",
+            height:'5.9'
+        }
+    }
+
+}
+//console.log(user.fullName.Userfullname.height);
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+
+//const obj3 = Object.assign(obj1,obj2)
+//console.log(obj3);
+// const obj3 ={...obj1,...obj2}
+// console.log(obj3);
+
+
