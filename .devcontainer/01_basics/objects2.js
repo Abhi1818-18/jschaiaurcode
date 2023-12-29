aef755f52f10ccce70d10ced10c0b1b1265ad508
Abhi1818-18 <131249108+Abhi1818-18@users.noreply.github.com>
2023-12-29 09:22:06 +0000

    changes done

--- .devcontainer/01_basics/objects2.js
+++ .devcontainer/01_basics/objects2.js
@@ -13,6 +13,7 @@ console.log(Object.entries(tinderUser));
 console.log(Object.hasOwnProperty('loction')); //tells whether certain property exist or not
 
 
+
 const user ={
     email : "abhi@gmail.com",
     fullName:{
@@ -34,3 +35,15 @@ const obj2 = {3:"a",4:"b"}
 // console.log(obj3);
 
 
+const course={
+    name:"Spring boot",
+    duration:"6 months",
+    price : "10000"
+}
+
+const{price} = course //object deconstructing
+
+console.log(price);
+
+const{price:p} = course //to give small and concise name to property
+console.log(p);
